6465a590f8002af9de813b5116fa0801540fef29
e <qqqdyz@gmail.com>
2023-05-16 11:41:10 +0800

    home page + basic setup
    home page + basic setup

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "test"
-
+    return render_template("home.html")
 app.run()
-
-test
