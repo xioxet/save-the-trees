5fd43bbd84498c6cb60e86b7becd811939d6006e
oxedoh <121850167+oxedoh@users.noreply.github.com>
2023-05-19 10:41:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template("home.html")
-app.run()
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
     
+
+if __name__ == '__main__':
+    app.run()
