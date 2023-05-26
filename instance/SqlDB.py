ee9340f1bcc062841e1bfeee6e53a70dc2f51e7f
manaoming <111954567+manaoming@users.noreply.github.com>
2023-05-26 10:34:31 +0800

    Added sell product function, fixed update field function. All functions tested.

--- instance/SqlDB.py
+++ instance/SqlDB.py
@@ -15,14 +15,14 @@ def add_user():
     pass
 
 
-def add_product(prod_id, prod_name, unit_price, description, stock=0):
+def add_product(prod_id, prod_name, unit_price, description, stock=0, onsale=0):
     insert_product = ("INSERT INTO products "
-                      "(prod_id, prod_name, unit_price, description, stock, amt_sold)"
-                      "VALUES (%s, %s, %s, %s, %s, DEFAULT(amt_sold))")
+                      "(prod_id, prod_name, unit_price, description, stock, amt_sold, onsale)"
+                      "VALUES (%s, %s, %s, %s, %s, DEFAULT(amt_sold), %s)")
 
     if (len(prod_name) <= 45 and unit_price < 10000 and len(description) <= 90
-            and len(search_product(prod_id, "prod_id")) == 0):
-        product_params = (prod_id, prod_name, unit_price, description, stock)
+            and len(search_product(prod_id, "prod_id")) == 0) and onsale in (0, 1):
+        product_params = (prod_id, prod_name, unit_price, description, stock, onsale)
         mycursor.execute(insert_product, product_params)
         return True
     else:
@@ -50,12 +50,34 @@ def add_stock(prod_id, stock):
         return False
 
 
+def sell_product(prod_id, amount):
+    products = search_product(prod_id, fields="prod_id, unit_price, stock, onsale")
+    print(products)
+    if len(products) != 1:
+        print("Product does not exist")
+    elif products[0][2] < amount:
+        print("Insufficient stock")
+    elif products[0][3] != 1:
+        print("Product not on sale")
+    else:
+        sell_query = "UPDATE products SET stock = stock - %s WHERE prod_id = %s"
+        for product in products:
+            mycursor.execute(sell_query, (amount, product[0]))
+            print("Sold", amount, "of", product[0], ".", product[2]-amount, "remains")
+            print("Cost is", product[1]*amount)
+
+
 def update_field(prod_id, field, value):
     products = search_product(prod_id, fields="prod_id")
-    if len(products) > 0:
-        update_query = "UPDATE products SET %s = %s WHERE prod_id = %s"
+    fields = ("prod_name", "unit_price", "description", "stock", "amt_sold", "onsale")
+    if field not in fields:
+        print("Invalid column name")
+    elif len(products) != 1:
+        print("Product does not exist")
+    else:
+        update_query = "UPDATE products SET " + field + " = %s WHERE prod_id = %s"
         for product in products:
-            mycursor.execute(update_query, (field, value, product[0]))
+            mycursor.execute(update_query, (value, product[0]))
     pass
 
 
@@ -77,11 +99,11 @@ if __name__ == "__main__":
         print('username: ', users[0])
         print('password: ', users[1])
 
-    add_product(1, "Product 1", 123.45, "Product 1 is the first product by Buy A Tree", 10)
+    print("Add Product Test")
+    add_product(1, "Product 1", 123.45, "Product 1 is the first product by Buy A Tree", 10, 1)
     mycursor.execute('select * from products')
-    print(mycursor.fetchall())
-    for product in mycursor.fetchall():
-        print(product)
+    for test_product in mycursor.fetchall():
+        print(test_product)
 
     print("Search test")
     print(search_product(2))
@@ -91,4 +113,17 @@ if __name__ == "__main__":
     print("Add Stock Test")
     print(add_stock(2, 5))
     print(search_product(2, fields="stock"))
-    mydb.rollback()
+
+    print("Update Field Test")
+    print(update_field(2, 'onsale', 1))
+    print(search_product(2, fields="onsale"))
+
+    print("Sell Product Test")
+    sell_product(2, 5)
+    print(search_product(2, fields="stock"))
+
+    print("Delete Product Test")
+    delete_product(2)
+    mycursor.execute('select * from products')
+    for test_product in mycursor.fetchall():
+        print(test_product)
