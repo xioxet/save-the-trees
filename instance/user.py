e7f28b58f500c0730423155ee47a979ed6802700
e <qqqdyz@gmail.com>
2023-08-01 21:13:32 +0800

    Revert "Update user.py"
    This reverts commit 7534cf9f4be1274d76b43c0a1e494d48a52c0dd1.

--- instance/user.py
+++ instance/user.py
@@ -1,9 +1,8 @@
 from instance import mydb, mycursor
 import datetime
-from secrets import token_urlsafe
+import bcrypt
 
 token_expiration_time = datetime.timedelta(minutes=5)
-
 
 def find_username(username):
     query = "SELECT * FROM users WHERE username = %s"
@@ -13,7 +12,6 @@ def find_username(username):
     if len(data) == 0: return None
     return data[0]
 
-
 def find_email(email):
     query = "SELECT * FROM users WHERE email = %s"
     mycursor.execute(query, (email,))
@@ -22,34 +20,12 @@ def find_email(email):
     if len(data) == 0: return None
     return data[0]
 
-
 def add_user(username, password, email):
-    #Find the current maximum User ID in the "users" table
-    max_user_id_query = "SELECT MAX(user_id) FROM users"
-    mycursor.execute(max_user_id_query)
-    max_user_id = mycursor.fetchone()[0]
-
-    #Calculate the next available User ID by adding 1 to the maximum User ID
-    next_user_id = max_user_id + 1
-
-    #Insert the new row with the calculated User ID
     insert_user = ("INSERT INTO users"
-                   "(user_id, username, password, email, role)"
-                   "VALUES (%s, %s, %s, %s, %s)")
-
-    user_params = (next_user_id, username, password, email, 'user')
-
+                      "(username, password, email, role)"
+                      "VALUES (%s, %s, %s, %s)")
+    user_params = (username, password, email, 'user')
     mycursor.execute(insert_user, user_params)
-    mydb.commit()
-
-def delete_user(username):
-    query = "DELETE FROM users WHERE username = %s"
-    mycursor.execute(query, (username,))
-    mydb.commit()
-
-def update_user(username, password, email):
-    query = "UPDATE FROM users WHERE username = %s"  # Corrected column name to username
-    mycursor.execute(query, (username,))
     mydb.commit()
 
 def check_role(username):
@@ -60,23 +36,21 @@ def check_role(username):
         return data[0][0]
     return None
 
-
 def add_verification_token(verification_token, username, password, email):
     date = datetime.datetime.now().date().isoformat()
-    query = "INSERT INTO users (token, username, password, email, date) VALUES (%s, %s, %s, %s, %s)"
+    query = "INSERT INTO signups (token, username, password, email, date) VALUES (%s, %s, %s, %s, %s)"
     mycursor.execute(query, (verification_token, username, password, email, date))
     mydb.commit()
 
-
 def verify_token(token):
-    query = "SELECT username, password, email, date FROM users WHERE token = %s"
+    query = "SELECT username, password, email, date FROM signups WHERE token = %s"
     mycursor.execute(query, (token,))
 
     try:
         username, password, email, date = [row for row in mycursor][0]
     except:
         raise Exception("Invalid verification.")
-
+    
     date_difference = datetime.datetime.now() - date
 
     if date_difference < token_expiration_time:
@@ -84,9 +58,22 @@ def verify_token(token):
 
     else:
         add_user(username, password, email)
-        query = "DELETE FROM users WHERE token = %s"
+        query = "DELETE FROM signups WHERE token = %s"
         mycursor.execute(query, (token,))
         mydb.commit()
+    
 
-
+def update_passwords():
+    query = "SELECT * FROM users"
+    mycursor.execute(query)
+    data = [row for row in mycursor]
+    for row in data:
+        user_id = row[0]
+        password = row[2]
+        salt = bcrypt.gensalt()
+        hash = bcrypt.hashpw(password.encode('utf-8'), salt)
+        query = "UPDATE users SET password = %s WHERE user_id = %s"
+        values = (hash.decode('utf-8'), user_id)
+        mycursor.execute(query, values)
+        mydb.commit()
 
