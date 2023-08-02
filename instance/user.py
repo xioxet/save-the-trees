3ad9f45aff57eb8d8fe92d8a5c07eee76b28e27d
oxedoh <121850167+oxedoh@users.noreply.github.com>
2023-08-03 07:18:06 +0800

    user id
    user id auto increments now so no more errors when signing up

--- instance/user.py
+++ instance/user.py
@@ -13,6 +13,12 @@ def find_username(username):
     if len(data) == 0: return None
     return data[0]
 
+def find_user_verify(username):
+    query = "SELECT user_id, password, email, role, is_verified FROM users WHERE username = %s"
+    mycursor.execute(query, (username,))
+    user_details = mycursor.fetchone()
+    return user_details
+
 
 def find_email(email):
     query = "SELECT * FROM users WHERE email = %s"
@@ -24,20 +30,12 @@ def find_email(email):
 
 
 def add_user(username, password, email):
-    #Find the current maximum User ID in the "users" table
-    max_user_id_query = "SELECT MAX(user_id) FROM users"
-    mycursor.execute(max_user_id_query)
-    max_user_id = mycursor.fetchone()[0]
-
-    #Calculate the next available User ID by adding 1 to the maximum User ID
-    next_user_id = max_user_id + 1
-
     #Insert the new row with the calculated User ID
     insert_user = ("INSERT INTO users"
-                   "(user_id, username, password, email, role)"
-                   "VALUES (%s, %s, %s, %s, %s)")
+                   "(username, password, email, role)"
+                   "VALUES (%s, %s, %s, %s)")
 
-    user_params = (next_user_id, username, password, email, 'user')
+    user_params = (username, password, email, 'user')
 
     mycursor.execute(insert_user, user_params)
     mydb.commit()
@@ -60,7 +58,7 @@ def check_role(username):
         return data[0][0]
     return None
 
-
+#signup_verification
 def add_verification_token(verification_token, username, password, email):
     date = datetime.datetime.now().date().isoformat()
     query = "INSERT INTO users (token, username, password, email, date) VALUES (%s, %s, %s, %s, %s)"
@@ -88,5 +86,31 @@ def verify_token(token):
         mycursor.execute(query, (token,))
         mydb.commit()
 
+#login_verification
+def add_verification_pin(verification_pin):
+    date = datetime.datetime.now().date().isoformat()
+    query = "INSERT INTO users (verification_pin, date) VALUES (%s, %s)"
+    mycursor.execute(query, (verification_pin, date))
+    mydb.commit()
+
+def verify_pin(pin):
+    query = "SELECT username, date FROM users WHERE verification_pin = %s"
+    mycursor.execute(query, (pin,))
+
+    try:
+        username, date = [row for row in mycursor][0]
+    except:
+        raise Exception("Invalid verification.")
+
+    date_difference = datetime.datetime.now() - date
+
+    if date_difference < token_expiration_time:
+        raise Exception("Timed out.")
+
+    else:
+        query = "DELETE FROM users WHERE verification_pin = %s"
+        mycursor.execute(query, (pin,))
+        mydb.commit()
+        return username
 
 
